e25e620e5db433424c038fe6bdb5ae8c50ea5247
Kyle Stevens <kyleastevens3@gmail.com>
2023-02-25 15:57:30 -0800

    Made changes to basic-server and request-handler

--- server/request-handler.js
+++ server/request-handler.js
@@ -28,66 +28,79 @@ var defaultCorsHeaders = {
   'access-control-max-age': 10 // Seconds.
 };
 
+const messages = [];
+
 
 module.exports.requestHandler = function(request, response) {
-  // Request and Response come from node's http module.
-  //
-  // They include information about both the incoming request, such as
-  // headers and URL, and about the outgoing response, such as its status
-  // and content.
-  //
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/`
 
-  // Do some basic logging.
-  //
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // console.logs in your code.
   console.log('Serving request type ' + request.method + ' for url ' + request.url);
 
-  const { method, url } = request;
+  var statusCode;
 
-  // request.on('error', (err) => {
-  //   // This prints the error message and stack trace to `stderr`.
-  //   console.error(err.stack);
-  // });
-
-  // The outgoing status.
-  var statusCode = 200;
-
-  if (method === 'POST') {
+  if (request.method === 'GET') {
+    statusCode = 200;
+  } else if (request.method === 'POST') {
     statusCode = 201;
+    var body = [];
+    request.on('error', (err) => {
+      console.error(err);
+    }).on('data', (chunk) => {
+      body.push(chunk);
+    }).on('end', () => {
+      body = Buffer.concat(body).toString();
+      messages.push(JSON.parse(body));
+    });
   }
+  var responseBody = messages;
 
-  if (url !== '/classes/messages') {
+  if (request.url !== '/classes/messages') {
     statusCode = 404;
   }
 
-  // See the note below about CORS headers
   var headers = defaultCorsHeaders;
-
-  // Tell the client we are sending them plain text.
-  //
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
   headers['Content-Type'] = 'application/json';
 
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
   response.writeHead(statusCode, headers);
-
-  const responseBody = { headers, method, url };
-
-  response.write(JSON.stringify([].concat(responseBody)));
-
-  // Make sure to always call response.end() - Node may not send
-  // anything back to the client until you do. The string you pass to
-  // response.end() will be the body of the response - i.e. what shows
-  // up in the browser.
-  //
-  // Calling .end "flushes" the response's internal buffer, forcing
-  // node to actually send all the data over to the client.
-  response.end();
+  response.end(JSON.stringify(responseBody));
 };
+
+
+// Tell the client we are sending them plain text.
+//
+// You will need to change this if you are sending something
+// other than plain text, like JSON or HTML.
+
+// .writeHead() writes to the request line and headers of the response,
+// which includes the status and all headers.
+
+// Make sure to always call response.end() - Node may not send
+// anything back to the client until you do. The string you pass to
+// response.end() will be the body of the response - i.e. what shows
+// up in the browser.
+//
+// Calling .end "flushes" the response's internal buffer, forcing
+// node to actually send all the data over to the client.
+
+// Request and Response come from node's http module.
+//
+// They include information about both the incoming request, such as
+// headers and URL, and about the outgoing response, such as its status
+// and content.
+//
+// Documentation for both request and response can be found in the HTTP section at
+// http://nodejs.org/documentation/api/`
+
+// Do some basic logging.
+//
+// Adding more logging to your server can be an easy way to get passive
+// debugging help, but you should always be careful about leaving stray
+// console.logs in your code.
+
+// const { method, url } = request;
+
+// request.on('error', (err) => {
+//   // This prints the error message and stack trace to `stderr`.
+//   console.error(err.stack);
+// });
+
 
